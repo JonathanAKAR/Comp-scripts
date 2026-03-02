983fb509a401172860930b50fe159846cb4382f3
illu <jo.kartawidjaja@gmail.com>
2026-03-01 21:32:04 -0500

    corrected harden scripts

--- general/hardening.sh
+++ general/hardening.sh
@@ -15,17 +15,6 @@ sudo apt install git -y #add git
 sudo apt install tmux #add tmux
 sudo apt install ssh #add ssh
 
-#add backup user
-adduser backup_user
-
-#basic ssh config
-sed -i 's/^#*PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
-systemctl enable ssh || systemctl enable sshd
-
-#just in case
-systemctl start ssh || systemctl start sshd
-systemctl restart ssh || systemctl restart sshd
-
 #make sure we have no ssh key
 rm /root/.ssh/authorized_keys
 rm /home/*/.ssh/authorized_keys
