5145a6212c62881f446635615e6fb6d30266fad0
illu <jo.kartawidjaja@gmail.com>
2026-03-04 11:06:06 -0500

    added backup script

--- backup/backup_from_web.sh
+++ backup/backup_from_web.sh
@@ -4,24 +4,32 @@
 BACKUP_ROOT="/var/backups/www"
 TMP_DIR="/tmp/server_backup_temp"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-mkdir -p "$BACKUP_ROOT" "$TMP_DIR"
-REMOTE_USER="backup_user"
 
-# Replace with actual IPs
+
+# === CHANGEABLE ===
+REMOTE_USER="bk_user" #CHANGE ME
+BACKUP_ROOT="/home/${REMOTE_USER}"
 BACKUP_HOST="" #CHANGE_ME
 
 
-if [ -z $WEB_HOST || $UID -ne 0 ]; then
-	echo "Please change the default IP && run this script as root"
+if [ -z $BACKUP_HOST ]; then
+	echo "Please change the default IP"
+	exit 1
 fi
+if [ $UID -ne 0 ]; then
+        echo "run this script as root"
+        exit 1
+fi
+
+
 
 # === FUNCTIONS ===
 
 backup_web_server() {
     echo "Backing up Web Server from $WEB_HOST..."
     sudo tar -czf /tmp/web_config.tar.gz /etc/apache2 /var/www/html
-    ssh "$REMOTE_USER@$BACKUP_HOST" 'mkdir -p "$BACKUP_ROOT"'
-    scp /tmp/web_config.tar.gz "$REMOTE_USER@$BACKUP_HOST:/$BACKUP_ROOT/web_backup_$DATE.tar.gz"
+    ssh "$REMOTE_USER@$BACKUP_HOST" "mkdir -p ${BACKUP_ROOT}"
+    scp /tmp/web_config.tar.gz ${REMOTE_USER}@${BACKUP_HOST}:${BACKUP_ROOT}/web_backup_$DATE.tar.gz
     rm /tmp/web_config.tar.gz
     echo "Web server backup saved to $BACKUP_ROOT/web_backup_$DATE.tar.gz"
 }
